5713da5e76d570fbb346ef3e86da0382383502c0
Pooja Softradix <softradix@Poojas-MacBook-Air.local>
2022-09-11 20:15:58 +0530

    web view UI

---
+++ Peachtree/Controllers/Home/Sub View Controllers/Pools/PoolsVC.swift
@@ -0,0 +1,18 @@
+//
+//  PoolsVC.swift
+//  Peachtree
+//
+//  Created by Pooja Softradix on 11/09/22.
+//
+
+import UIKit
+
+class PoolsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
