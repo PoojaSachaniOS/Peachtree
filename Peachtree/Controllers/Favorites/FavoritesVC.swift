e6449cf29b68f93d05c32af20370b79d267bcb7c
Anand <anand@Anands-MacBook-Pro.local>
2022-09-11 20:26:08 +0530

    update

--- Peachtree/Controllers/Favorites/FavoritesVC.swift
+++ Peachtree/Controllers/Favorites/FavoritesVC.swift
@@ -12,20 +12,13 @@ class FavoritesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.registerNib()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func registerNib() {
+        self.tblVwFavorites.register(FavoritesTableCell.self)
     }
-    */
 
 }
 
