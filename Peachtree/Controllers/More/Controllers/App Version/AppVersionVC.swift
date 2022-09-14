8834897199db8cf7c3eb037a9a036f0541fa163f
Anand <anand@Anands-MacBook-Pro.local>
2022-09-14 21:21:49 +0530

    update

--- Peachtree/Controllers/More/Controllers/App Version/AppVersionVC.swift
+++ Peachtree/Controllers/More/Controllers/App Version/AppVersionVC.swift
@@ -12,7 +12,6 @@ class AppVersionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func backTaped(_ sender: Any) {
